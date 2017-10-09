1469f5a0a62c896a2c92b2bd187c911ad657f183
Larry Luk <larryluk526@gmail.com>
2017-10-09 17:32:03 -0400

    Final

--- FizzBuzz/main.c
+++ FizzBuzz/main.c
@@ -9,7 +9,22 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    printf("Hello, World!\n");
-    return 0;
+   
+    for (int i = 1; i < 101; i++) {
+        
+        if ((i % 3) == 0 && (i % 5) == 0) {
+        printf("FizzBuzz \n");
+        } else if ((i % 3) == 0) {
+        printf("Fizz \n");
+        } else if ((i % 5) == 0) {
+        printf("Buzz \n");
+        } else {
+           
+        printf("%d \n", i);
+
+        }
+        
+
+    }
     
 }
